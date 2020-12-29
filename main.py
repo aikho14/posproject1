8eae5b6ab82f280a3c8945495d6bee85163f1299
AL <aledward.arcilla@gmail.com>
2020-12-29 19:29:16 +0100

    working table

--- main.py
+++ main.py
@@ -24,11 +24,14 @@ class Another_Main(QMainWindow, FORM_x1):
         QMainWindow.__init__(self)
         self.setupUi(self)
         self.Handle_Buttons()
+        
+    
     
  
 #Button Functions 
     def Handle_Buttons(self):
         pass
+
     
    
     
@@ -38,63 +41,64 @@ class Main(QMainWindow, FORM_x):
         super(Main,self).__init__(parent)
         QMainWindow.__init__(self)
         self.setupUi(self)
+        
         self.Handle_Buttons()
         self.combobox_fill()
- 
+        
+        
+    
+ #######new
 #Button Functions 
-    def Handle_Buttons(self):
-        self.addon_button.clicked.connect(self.add_button)
+    def Handle_Buttons(self):            
+        self.addon_button.clicked.connect(self.ben)
 
 
 
-    def combobox_fill(self):        
+  
+        
+    def combobox_fill(self):
+                   
         db=sqlite3.connect("countrywingsdatabase.db") #database file
         cursor=db.cursor()
         
         command= ''' SELECT item_name from inventory''' # db command line filter
         result = cursor.execute(command)
-    
-        items  = []# list items in inventory
-        #loop to insert items to combo box
-        for items_inbox in result.fetchall():
-            items.append(items_inbox[0])
-
-        for x  in items:
+        
+        data = []
+        # data = ['A','C','D','B']        
+        for row in result.fetchall():
+            data.append(row[0])
+        # self.addon_listbox['values'] = data
+        # return data
+        # self.addon_listbox.items(data)
+        for x in data:
             self.addon_listbox.addItem(x)
     
-    
-    
-    def add_button(self):
+    def ben(self):
         db=sqlite3.connect("countrywingsdatabase.db") #database file
-        cursor2=db.cursor()
-        
-        addon_text= str(self.addon_listbox.currentText()) 
-        
-        getprice= ''' SELECT item_price from inventory where item_name =?''' # db command line filter
-        
-        resultitem_price=cursor2.execute(getprice,[addon_text])
-        
-        item_price=str(resultitem_price.fetchone()[0])
-        
-        addon_list=[addon_text, item_price]
-        
-        table=self.addon_table
+        cursor2=db.cursor()        
+        addon_text= str(self.addon_listbox.currentText())         
+        getprice= ''' SELECT item_price from inventory where item_name =?''' # db command line filter        
+        resultitem_price=cursor2.execute(getprice,[addon_text])        
+        item_price=str(resultitem_price.fetchone()[0])        
+        addon_list = [ addon_text, item_price ]        
+        # addon_list = ['1','12323']
+        table = self.addon_table        
         self.addTableRow(table , addon_list) 
+            
+    
+    def addTableRow(self, addon_table, row_data):
         
-        print (addon_list)
         
-    def addTableRow(self, addon_table, addon_list):
-        
-         
-         row=addon_table.rowCount()
-         addon_table.setRowCount(row+1)
+        row = addon_table.rowCount()
+        addon_table.setRowCount(row+1)
+        col = 0
+        for item in row_data:
+            cell = QTableWidgetItem(str(item))
+            addon_table.setItem(row, col, cell)
+            col += 1
 
-
-
-
-
-
-
+    
 
 
 
@@ -105,9 +109,10 @@ class Main(QMainWindow, FORM_x):
 
 def main():
         app=QApplication(sys.argv)
-        window=Main()     
+        window=Main()
+        
         window.show()
-        app.exec_()  
+        app.exec_()          
         
 
 def mains():
